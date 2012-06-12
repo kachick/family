de4bdcb7d9014aeb966b92b61d67721d7ea32dd2
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-06-13 03:40:03 +0900

    draft

---
+++ examples/example.rb
@@ -0,0 +1,13 @@
+$VERBOSE = true
+
+require_relative '../lib/family'
+
+list = Family.new Integer
+list << 7 #=> 7
+#~ p list << 1.0 #=> Exception
+p list << 1 #=> 1
+p list.inspect #=> Integer[7, 1]
+
+list = Family.define{AND(String, /\d/)}
+#~ list << 'k'
+list << '8'
